9b8177de5bb39314ad8f44266b4a586f1875a2b2
Bravin Thillainathan <bravint@gmail.com>
2021-10-27 21:00:24 +0100

    js: prep for .map (for loop now acts on function)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-// - Make sure you check and understand the data that is given to you!
-// - Create a card using JS that represents a single pokemon, use the example image as a reference. You will also find an HTML example commented out in the index.html
-// - Use the exact CSS classes you see in the example HTML to obtain the same style for each card
-// - The cards should be nested inside <ul class="cards"></ul>
-// - Use the official-artwork object key as the images for the card. The images are all inside of the sprites key, in each pokemon object
-// pokemon.sprites.other['official-artwork'].front_default
-
-// - Render all the cards on the page that represents all the pokemons, recreating the same layout, using JS
-
-function CapitaliseName (a) {
-    let lowercaseName = a;
+function CapitaliseName(PokemonName) {
+    let lowercaseName = PokemonName;
     let firstLetterLowercase = lowercaseName.charAt(0);
     let firstLetterUppercase = firstLetterLowercase.toUpperCase();
     let restOfword = lowercaseName.slice(1);
@@ -22,28 +13,32 @@ function createStatsList(statName, statValue) {
     return li;
 }
 
-for (i=0; i < data.length; i++) {
+function createCard (a) {
     const card = document.createElement('li');
     card.className = 'card';
-    document.querySelector('.cards').append(card)
-    
+
     const pokemonName = document.createElement('h2');
     pokemonName.className = 'card--title';
-    pokemonName.innerText = CapitaliseName(data[i].name);
+    pokemonName.innerText = CapitaliseName(data[a].name);
     card.append(pokemonName);
-    
+
     const image = document.createElement('img');
     image.className = 'card--img';
-    image.setAttribute("src", data[i].sprites.other['official-artwork'].front_default);
+    image.setAttribute("src", data[a].sprites.other['official-artwork'].front_default);
     image.setAttribute("width", 256);
     card.append(image);
-    
+
     const text = document.createElement('ul');
     text.className = 'card--text';
     card.append(text);
-    for (j = 0; j < data[i].stats.length; j++) {
-        let statName = data[i].stats[j].stat.name;
-        let statValue = data[i].stats[j].base_stat;
-        text.append(createStatsList(statName, statValue))
-    }
+        for (j = 0; j < data[a].stats.length; j++) {
+            let statName = data[a].stats[j].stat.name;
+            let statValue = data[a].stats[j].base_stat;
+            text.append(createStatsList(statName, statValue));
+        } 
+    return card
+}
+
+for (i=0; i < data.length; i++) {
+    document.querySelector('.cards').append(createCard(i));
 }
